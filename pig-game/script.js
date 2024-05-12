cb32dc07d316ba9ad92347c9e742bba5a41f7ab7
John Ensberg <John.Ensberg@gmail.com>
2024-05-13 01:54:46 +0200

    do not declare snd

--- pig-game/script.js
+++ pig-game/script.js
@@ -2,13 +2,16 @@
 // TODO Implement turn counter for both players
 // TODO Reverse current & total score
 
+// FIXME Weird audio problem in browsers. Does work in devOp.
 // Setting up sounds
+/*
 const sndWinGame = new Audio('/snd/win-game.mp3');
 const sndGameOver = new Audio('/snd/game-over.mp3');
 const sndError = new Audio('/snd/fout.mp3');
 const sndSwitchPlayer = new Audio('/snd/yoink.mp3');
 const sndRollDice = new Audio('/snd/dice-rolling.mp3');
 const sndNewGame = new Audio('/snd/newGame.mp3');
+*/
 
 // Selecting Elements
 const elScore0 = document.getElementById('score--0');
