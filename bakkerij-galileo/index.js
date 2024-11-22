9b22cd92484972f85469cd09c1123b95b8a9df52
John Ensberg <John.Ensberg@gmail.com>
2024-11-22 11:59:57 +0100

    Update index.js

--- bakkerij-galileo/index.js
+++ bakkerij-galileo/index.js
@@ -3,22 +3,20 @@
 //Selecteer alle nav links
 const headerNav = document.querySelector("header .nav");
 const links = headerNav.querySelectorAll(".nav-list a");
+const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+const phoneRegex = /^(\+31|0)(6[\-]?\d{8})$/;
 
-console.log(links);
-console.log(links[1].href);
-console.log(links[1].baseURI);
+//  Test voor code
+// console.log(links);
+// console.log(links[1].href);
+// console.log(links[1].baseURI);
 
 for (let i = 0; i < links.length; i++) {
-  // const linkPath = links[i].getAttribute("href").replace(/\/$/, "");
-
   if (links[i].href === links[i].baseURI) {
     links[i].classList.add("active");
     break;
   }
 }
-
-const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-const phoneRegex = /^(\+31|0)(6[\-]?\d{8})$/;
 
 document.addEventListener("DOMContentLoaded", function () {
   const currentPath = window.location.pathname;
@@ -26,14 +24,17 @@ document.addEventListener("DOMContentLoaded", function () {
   // Registreer de pagina in de console
   console.log(`De gebruiker bevindt zich op: ${currentPath}`); //check of t klopt
 
+  // Controleer juiste pagina om elementen te kunnen selecteren
   if (currentPath === "/contact.html") {
-    console.log("We zijn er!");
+    // Eerst de errors selecteren
     const errorMessageMail = document.getElementById("error-message-mail");
     const errorMessagePhone = document.getElementById("error-message-phone");
 
+    // Check of je de elementen hebt
     console.log(errorMessageMail);
     console.log(errorMessagePhone);
 
+    // Error meldingen uitzetten
     errorMessageMail.style.display = errorMessagePhone.style.display = "none";
 
     document
@@ -44,6 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
         let emailInput = document.getElementById("email").value;
         let phoneInput = document.getElementById("phone").value;
 
+        // Test de gevoelige input.Iniden nodig : error!
         if (emailRegex.test(emailInput)) {
           errorMessageMail.style.display = "none";
         } else {
