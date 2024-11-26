a43be8fe5f3fbc66b2641d0a40ad8a50667939aa
John Ensberg <John.Ensberg@gmail.com>
2024-11-27 00:15:26 +0100

    hamburger menu

--- bakkerij-galileo-sem/index.js
+++ bakkerij-galileo-sem/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 (function onLoad() {
-	// Regex codes for input validation
+  // Regex codes for input validation
   const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   const phoneRegex = /^(\+31|0)(6[\-]?\d{8})$/;
 
@@ -9,43 +9,51 @@
     const linkPath = link.getAttribute("href").replace(/\/$/, "");
     const currentPath = window.location.pathname.replace(/\/$/, "");
 
-// Dev check data
+    // Dev check data
     console.log(linkPath);
     console.log(currentPath);
 
-  // Assigns the 'active' class to the navigational links
+    // Assigns the 'active' class to the navigational links
     if (currentPath.endsWith(linkPath)) {
       link.classList.add("active");
     }
   });
-  
-  
+
+  // Hamburger mobiel
+  const navToggle = document.getElementById("nav-toggle");
+  const navList = document.querySelector(".nav-list");
+
+  navToggle.addEventListener("click", () => {
+    navList.classList.toggle("active"); // Toon/verberg het menu
+    navToggle.classList.toggle("open"); // Animeren van het icoon
+  });
+
   let contactForm = document.getElementById("form-contact");
 
-// Check if on contact page
+  // Check if on contact page
   if (contactForm) {
     contactForm.addEventListener("submit", function onSubmit(event) {
       event.preventDefault();
 
-// Select error msgs  
+      // Select error msgs
       const errorMessageMail = document.getElementById("error-message-mail");
       const errorMessagePhone = document.getElementById("error-message-phone");
 
-// Make errors invisible
+      // Make errors invisible
       errorMessageMail.style.display = errorMessagePhone.style.display = "none";
 
-// Get input
+      // Get input
       let emailInput = document.getElementById("email").value;
       let phoneInput = document.getElementById("phone").value;
 
-// Check input email
+      // Check input email
       if (emailRegex.test(emailInput)) {
         errorMessageMail.style.display = "none";
       } else {
         errorMessageMail.style.display = "block";
       }
 
-// Check input phone number
+      // Check input phone number
       if (phoneRegex.test(phoneInput)) {
         errorMessagePhone.style.display = "none";
       } else {
@@ -58,5 +66,3 @@
     });
   }
 })();
-
-  
