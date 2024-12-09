d34d1cfcf415f75083ecc4ada7b79071df4942e3
John Ensberg <John.Ensberg@gmail.com>
2024-12-09 14:43:36 +0100

    Update index.js

--- bakkerij-galileo/index.js
+++ bakkerij-galileo/index.js
@@ -1,68 +1,92 @@
 "use strict";
 
 (function onLoad() {
-  // Regex codes for input validation
-  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-  const phoneRegex = /^(\+31|0)(6[\-]?\d{8})$/;
+	// Regex codes for input validation
+	const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+	const phoneRegex = /^(\+31|0)(6[\-]?\d{8})$/;
 
-  document.querySelectorAll(".nav-list a").forEach((link) => {
-    const linkPath = link.getAttribute("href").replace(/\/$/, "");
-    const currentPath = window.location.pathname.replace(/\/$/, "");
+	const links = document.querySelectorAll(".nav-list a");
 
-    // Dev check data
-    // console.log(linkPath);
-    // console.log(currentPath);
+	links.forEach((link) => {
+		const linkPath = link.getAttribute("href").replace(/\/$/, "");
+		const currentPath = window.location.pathname.replace(/\/$/, "");
 
-    // Assigns the 'active' class to the navigational links
-    if (currentPath.endsWith(linkPath)) {
-      link.classList.add("active");
-    }
-  });
+		// Dev check data
+		// console.log(linkPath);
+		// console.log(currentPath);
 
-  let contactForm = document.getElementById("form-contact");
+		// Assigns the 'active' class to the navigational links
+		if (currentPath.endsWith(linkPath)) {
+			link.classList.add("active");
+		}
+	});
 
-  // Check if on contact page
-  if (contactForm) {
-    contactForm.addEventListener("submit", function onSubmit(event) {
-      event.preventDefault();
+	let contactForm = document.getElementById("form-contact");
 
-      // Select error msgs
-      const errorMessageMail = document.getElementById("error-message-mail");
-      const errorMessagePhone = document.getElementById("error-message-phone");
+	// Check if on contact page
+	if (contactForm) {
+		contactForm.addEventListener("submit", function onSubmit(event) {
+			event.preventDefault();
 
-      // Make errors invisible
-      errorMessageMail.style.display = errorMessagePhone.style.display = "none";
+			// Select error msgs
+			const errorMessageMail = document.getElementById("error-message-mail");
+			const errorMessagePhone = document.getElementById("error-message-phone");
 
-      // Get input
-      let emailInput = document.getElementById("email").value;
-      let phoneInput = document.getElementById("phone").value;
+			// Make errors invisible
+			errorMessageMail.style.display = errorMessagePhone.style.display = "none";
 
-      // Check input email
-      if (emailRegex.test(emailInput)) {
-        errorMessageMail.style.display = "none";
-      } else {
-        errorMessageMail.style.display = "block";
-      }
+			// Get input
+			let emailInput = document.getElementById("email").value;
+			let phoneInput = document.getElementById("phone").value;
 
-      // Check input phone number
-      if (phoneRegex.test(phoneInput)) {
-        errorMessagePhone.style.display = "none";
-      } else {
-        errorMessagePhone.style.display = "block";
-      }
+			// Check input email
+			if (emailRegex.test(emailInput)) {
+				errorMessageMail.style.display = "none";
+			} else {
+				errorMessageMail.style.display = "block";
+			}
 
-      if (emailRegex.test(emailInput) && phoneRegex.test(phoneInput)) {
-        alert("Formulier succesvol verzonden!");
-      }
-    });
-  }
+			// Check input phone number
+			if (phoneRegex.test(phoneInput)) {
+				errorMessagePhone.style.display = "none";
+			} else {
+				errorMessagePhone.style.display = "block";
+			}
 
-  // Hamburger mobiel
-  const hamMenu = document.querySelector(".ham-menu");
-  const offScreenMenu = document.querySelector(".off-screen-menu");
+			if (emailRegex.test(emailInput) && phoneRegex.test(phoneInput)) {
+				alert("Formulier succesvol verzonden!");
+			}
+		});
 
-  hamMenu.addEventListener("click", () => {
-    hamMenu.classList.toggle("active");
-    offScreenMenu.classList.toggle("active");
-  });
+		const form = document.getElementById("form");
+	}
+
+	// Hamburger mobiel
+	const hamMenu = document.querySelector(".ham-menu");
+	const offScreenMenu = document.querySelector(".off-screen-menu");
+
+	hamMenu.addEventListener("click", () => {
+		hamMenu.classList.toggle("active");
+		offScreenMenu.classList.toggle("active");
+	});
+
+	// Check of form input heeft
+	function isFormUsed(form) {
+		const inputs = form.querySelectorAll("input");
+		return Array.from(inputs).some((input) => input.value.trim() !== "");
+	}
+
+	// Stop de hyperlink als er input in de form is
+	links.forEach((link) => {
+		link.addEventListener("click", function (event) {
+			if (isFormUsed(form)) {
+				const confirmation = confirm(
+					"U heeft niet-opgeslagen data in dit formulier. Weet u zeker dat u de pagina wilt verlaten? "
+				);
+				if (!confirmation) {
+					event.preventDefault();
+				}
+			}
+		});
+	});
 })();
